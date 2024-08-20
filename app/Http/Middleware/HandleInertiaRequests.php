4ab278befa421ebdcd379fb72cca637fb61e02e1
psalmistcom <dadewale9@gmail.com>
2024-08-20 14:18:25 +0100

    connect socket

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -36,7 +36,9 @@ class HandleInertiaRequests extends Middleware
             'auth' => [
                 'user' => $request->user(),
             ],
-            'conversations' => Auth::id() ? Conversation::getConversationsForSidebar(Auth::user()) : [],
+            'conversations' => Auth::id() ?
+                Conversation::getConversationsForSidebar(Auth::user()) :
+                [],
         ];
     }
 }
